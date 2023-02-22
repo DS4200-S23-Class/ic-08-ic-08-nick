7d430e7c2796ed20db08065d87146f3c44d5632d
nickhaugen19 <71619276+nickhaugen19@users.noreply.github.com>
2023-02-22 15:38:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
+// set the dimensions and margins of the graph
+var margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width = 600 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-
-var svg2 = d3.select("#my_bar_dataviz")
+// append the svg object to the div called 'my_bar_dataviz'
+var svg = d3.select("#my_bar_dataviz")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -11,35 +12,37 @@ var svg2 = d3.select("#my_bar_dataviz")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("https://raw.githubusercontent.com/DS4200-S23-Class/hw-05-hw05-nick-ethan/master/data/bar-data.csv", function(bardata) {
+// Parse the Data
+d3.csv("https://raw.githubusercontent.com/DS4200-S23-Class/ic-08-ic-08-nick/master/data/data.csv", function(data) {
 
-var xx = d3.scaleBand()
-  .range([ 0, width])
-  .domain(bardata.map(function(d) { return d.category; }))
-  .padding(0.2);
-svg2.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(xx))
-  .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
+  // X axis
+  var x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(data.map(function(d) { return d.year; }))
+    .padding(0.2);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
 
-var yy = d3.scaleLinear()
-  .domain([0, 100])
-  .range([ height, 0]);
-svg2.append("g")
-  .call(d3.axisLeft(yy));
-    
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, d3.max(data, function(d) { return +d.value; })])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
 
-svg2.selectAll("mybar")
-  .data(bardata)
-  .enter()
-  .append("rect")
-    .attr("x", function(d) { return xx(d.category); })
-    .attr("y", function(d) { return yy(d.amount); })
-    .attr("width", xx.bandwidth())
-    .attr("height", function(d) { return height - yy(d.amount); })
-    .attr("fill", "steelblue")
+  // Bars
+  svg.selectAll("mybar")
+    .data(data)
+    .enter()
+    .append("rect")
+      .attr("x", function(d) { return x(d.year); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.value); })
+      .attr("fill", "#69b3a2")
 
-    
 })
