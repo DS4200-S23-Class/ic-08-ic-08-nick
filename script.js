4e9b49fc6f989d080d2b59382fe7a74855e21ac3
nickhaugen19 <71619276+nickhaugen19@users.noreply.github.com>
2023-02-22 15:14:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+var svg2 = d3.select("#my_bar_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
+d3.csv("https://raw.githubusercontent.com/DS4200-S23-Class/hw-05-hw05-nick-ethan/master/data/bar-data.csv", function(bardata) {
+
+var xx = d3.scaleBand()
+  .range([ 0, width])
+  .domain(bardata.map(function(d) { return d.category; }))
+  .padding(0.2);
+svg2.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(xx))
+  .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
+
+var yy = d3.scaleLinear()
+  .domain([0, 100])
+  .range([ height, 0]);
+svg2.append("g")
+  .call(d3.axisLeft(yy));
+    
+
+svg2.selectAll("mybar")
+  .data(bardata)
+  .enter()
+  .append("rect")
+    .attr("x", function(d) { return xx(d.category); })
+    .attr("y", function(d) { return yy(d.amount); })
+    .attr("width", xx.bandwidth())
+    .attr("height", function(d) { return height - yy(d.amount); })
+    .attr("fill", "steelblue")
